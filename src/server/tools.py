d95c05a06c95df5b4521e8fa8ed648f3a2f9b73a
Pablo N. Dalmasso <pablodalmasso@gmail.com>
2015-04-18 10:35:20 -0300

    "Se agregan estructura del servidor"

---
+++ src/server/tools.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from socket import error
+from threading import Thread
+from server.setting import *
+
+
+class Client(Thread):
+    """
+    Servidor eco - reenvía todo lo recibido.
+    """
+
+    def __init__(self, conn, addr, client):
+        # Inicializar clase padre.
+        Thread.__init__(self)
+
+        self.conn = conn
+        self.addr = addr
+        self.client = client
+
+    def run(self):
+        self.conn.send(MSG_WELCOME_CLIENTE)
+        while True:
+            try:
+                input_data = self.conn.recv(1024)
+            except error:
+                print "[%s] Error de lectura." % self.name
+                break
+            else:
+                if input_data:
+                    if input_data == EXIT_OPTION:
+                        self.conn.close()
+                        print self.addr[0] + " se a desconectado."
+                        self.client.remove(self)
+                        print "--%s cliente conectados" % str(len(self.client))
+                        break
+                    else:
+                        for x in self.client:
+                            x.send_message(input_data)
+        self.conn.close()
+
+    def close(self):
+        """Método para cerrar el socket."""
+        self.conn.close()
+
+    def send_message(self, message):
+        msg_temp = str(self.addr[0]) + "[" + str(self.addr[1]) + "]: "
+        msg_temp += input_data
+        self.conn.send(
+            msg_temp
+        )
+        print msg_temp
